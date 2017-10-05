4abb163d943f1a75404c3f19dd3deb1a887082ad
Faras Sadek <fadel@acdc.int.seas.harvard.edu>
2017-10-05 12:19:33 -0400

    terminate cluster

--- jupyterhub_files/terminate_all_workers.py
+++ jupyterhub_files/terminate_all_workers.py
@@ -20,15 +20,52 @@ from time import sleep
 
 #################################################################################################
 
+# Note: DO NOT USE THIS SCRIPT UNLESS YOU KNOW WHAT ARE YOU DOING
+
+#################################################################################################
+
+
 with open("/etc/jupyterhub/server_config.json", "r") as f:
     SERVER_PARAMS = json.load(f) # load local server parameters
-
 ec2 = boto3.resource("ec2", region_name=SERVER_PARAMS["REGION"])
 
-servers = Server.select()
-instance_ids = [server.server_id for server in servers]
-# deletes user/worker instances
-ec2.instances.filter(InstanceIds=instance_ids).terminate()
-print("Deleting done")
+def delete_user_ec2(userid):
+    try:
+        server = Server.get_server(userid)
+        server_id = server.server_id
+        server.remove_server(server_id)
+        user_instance = ec2.Instance(server_id)
+        user_instance.terminate()
+    except:
+        print("no such user or server")
+        return 1
+    return 0
 
+def check_user_ec2(userid):
+    try:
+        server = Server.get_server(userid)
+        server_id = server.server_id
+        user_instance = ec2.Instance(server_id)
+        print("user %s , ec2= %s" %(userid, user_instance))
+    except:
+        print("no such user or server")
+        return 1
+    return 0
+
+def delete_all_users_ec2s():
+    servers = Server.select()
+    instance_ids = [server.server_id for server in servers]
+    # if instance_ids is empty then filter will return all the EC2s
+    # then all EC2s will be deleted , which is dangerous , therefore
+    # instance_ids should have value if terminate will be used.
+    if instance_ids:
+        # deletes user/worker instances
+        ec2.instances.filter(InstanceIds=instance_ids).terminate()
+    else:
+        print ("No instance IDs provided")
+    print("Deleting done")
+
+
+#Uncomment the section below
+#delete_all_users_ec2s()
 
